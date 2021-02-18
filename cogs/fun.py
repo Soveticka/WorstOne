c5fb8326f8e76bd7eb41baa7c161be043593f08b
Soveticka <matejkomanek@outlook.cz>
2021-02-18 22:43:51 +0100

    More nsfw to choose from

--- cogs/fun.py
+++ cogs/fun.py
@@ -17,20 +17,31 @@ hentai = variables.hentai
 everything = normal + hentai
 
 
-def increaseUsed(query):
+def increaseUsed(query, title=""):
     with open("./variables/commandUsage.json", "r") as f:
         used = json.load(f)
 
-    used[query] += 1
+    if query in used:
+        if title == "":
+            used[query] += 1
+        else:
+            if title in used[query]:
+                used[query][title] += 1
+            else:
+                used[query][title] = 1
+    else:
+        used[query] = 1
+
 
     with open("./variables/commandUsage.json", "w") as f:
         json.dump(used, f, indent=4)
 
 
 async def sendSubmission(ctx, subreddit, title):
-    submission = await subreddit.random()
-
-    increaseUsed(title)
+    try:
+        submission = await subreddit.random()
+    except AttributeError:
+        await sendSubmission(ctx, subreddit, title)
     try:
         if ".png" in submission.url or ".gif" in submission.url or ".jpg" in submission.url or ".gifv" in submission.url:
             embed = discord.Embed(
@@ -47,7 +58,7 @@ async def sendSubmission(ctx, subreddit, title):
             else:
                 await ctx.send(embed=embed)
         else:
-            if "youtube" in submission.url:
+            if "youtube" in submission.url or "discord.gg" not in submission or "xxxamazing.xyz":
                 await sendSubmission(ctx, subreddit, title)
             else:
                 await ctx.send(f"/r/{title}\n{submission.url}")
@@ -62,89 +73,6 @@ class Fun(commands.Cog):
     def __init__(self, client):
         self.bot = client
 
-    # @commands.group(description="Send NSFW image from Reddit")
-    # async def nsfw(self, ctx, query="list", mode=""):
-    #     channel = ctx.channel
-    #     normal = variables.normal
-    #     hentai = variables.hentai
-    #     everything = normal + hentai
-    #
-    #     if channel.nsfw:
-    #         if query == "list":
-    #             await ctx.message.delete()
-    #             await main._nsfw(ctx)
-    #             return
-    #         elif query == "random" or query in normal or query in hentai:
-    #             if query in normal:
-    #                 subreddit = await reddit.subreddit(query)
-    #                 title = query
-    #             elif query in hentai:
-    #                 subreddit = await reddit.subreddit(query)
-    #                 title = query
-    #
-    #             elif query == "random":
-    #                 if mode == "real":
-    #                     rnd = random.randrange(0, len(normal))
-    #                     subreddit = await reddit.subreddit(normal[rnd])
-    #                     title = normal[rnd]
-    #
-    #                 elif mode == "hentai":
-    #                     rnd = random.randrange(0, len(hentai))
-    #                     subreddit = await reddit.subreddit(hentai[rnd])
-    #                     title = hentai[rnd]
-    #
-    #                 elif mode == "all":
-    #                     rnd = random.randrange(0, len(everything))
-    #                     subreddit = await reddit.subreddit(everything[rnd])
-    #                     title = everything[rnd]
-    #                 elif mode == "":
-    #                     rnd = random.randrange(0, len(everything))
-    #                     subreddit = await reddit.subreddit(everything[rnd])
-    #                     title = everything[rnd]
-    #                 else:
-    #                     await ctx.message.delete()
-    #                     await main._nsfw(ctx)
-    #                     return
-    #             try:
-    #                 submission = await subreddit.random()
-    #             except Exception:
-    #                 print("error")
-    #
-    #             increseUsed(query)
-    #             try:
-    #                 if ".png" in submission.url or ".gif" in submission.url or ".jpg" in submission.url or ".gifv" in submission.url:
-    #                     embed = discord.Embed(
-    #                         colour=discord.Colour.random(),
-    #                         title=f"/r/{title}"
-    #                     )
-    #                     embed.set_footer(text=f"Requested by: {ctx.author}", icon_url=ctx.message.author.avatar_url)
-    #                     embed.set_image(url=submission.url)
-    #                     permalink = "https://reddit.com" + submission.permalink
-    #                     embed.set_author(name="Clickable link", url=permalink)
-    #
-    #                     if ".gifv" in submission.url:
-    #                         await ctx.send(f"/r/{title}\n{submission.url}")
-    #                     else:
-    #                         await ctx.send(embed=embed)
-    #                 else:
-    #                     if "youtube" in submission.url:
-    #                         await self.nsfw(ctx, query, mode)
-    #                     else:
-    #                         await ctx.send(f"/r/{title}\n{submission.url}")
-    #
-    #             except AttributeError:
-    #                 print(f"In submission were not all needed information")
-    #                 await self.nsfw(ctx, query, mode)
-    #         else:
-    #             await ctx.message.delete()
-    #
-    #             return
-    #     else:
-    #         await ctx.send("Příkaz pošli do nsfw kanálu :)")
-    #     try:
-    #         await ctx.message.delete()
-    #     except:
-    #         print("No message to delete")
 
     @commands.group(invoke_without_command=True)
     async def nsfw(self, ctx, query="", mode=""):
@@ -152,6 +80,7 @@ class Fun(commands.Cog):
 
             if ctx.channel.nsfw:
                 if query in everything:
+                    increaseUsed(query)
                     await sendSubmission(ctx, await reddit.subreddit(query), query)
                 elif query == "":
                     await self._nsfw_list(ctx)
@@ -176,6 +105,7 @@ class Fun(commands.Cog):
                 await main._nsfw(ctx)
                 return
 
+            increaseUsed("random", title)
             await sendSubmission(ctx, await reddit.subreddit(title), title)
         else:
             await ctx.send(f"\u274C{ctx.author.mention} - {ctx.message.content} can be called only in nsfw channel!")
--- variables/variables.py
+++ variables/variables.py
@@ -1,6 +1,16 @@
 normal = ["gonewild", "realgirls", "worldpacks", "celebnsfw", "asiansgonewild", "collegesluts",
           "petitegonewild", "bustypetite", "legalteens", "adorableporn", "breedingmaterial", "onlyfansgirls101",
           "milf", "porn", "tiktoknsfw", "pussy", "boobs", "tikthots", "tittydrop", "gonewild30plus",
-          "onlyfans101", "biggerthanyouthought", "nsfw_japan", "juicyasians", "cosplaygirls", "nsfwcosplay"]
+          "onlyfans101", "biggerthanyouthought", "nsfw_japan", "juicyasians", "cosplaygirls", "nsfwcosplay",
+          "sexybutnotporn", "nsfw", "realmoms", "18_19", "onmww", "gonewildcurvy", "snapchat_sluts", "ass",
+          "booty", "yogapants", "nipples", "tinytits", "braceface", "shorthairchicks", "girlswithneonhair", "feet",
+          "tightshorts", "datgap", "thighhighs", "hairypussy", "hotchickswithtattoos", "sexytummies", "volleyballgirls",
+          "bustyasians", "nsfw_korea", "kpopfap", "ginger", "latinacuties", "snowwhites", "lesbians", "bikinis",
+          "assinthong", "sexyvelma"]
+
 hentai = ["hentai", "rule34", "hentai_gif", "animemilfs", "yuri", "monstergirl",
-          "nintendowaifus", "hentaibondage", "cosplaygirls", "nsfwcosplay", "thick_hentai"]
+          "nintendowaifus", "hentaibondage", "cosplaygirls", "nsfwcosplay", "thick_hentai", "rule34_ass",
+          "rule34bondage", "imaginaryboners", "rule34feet", "rule34pinups", "westernhentai", "2booty", "arknuts",
+          "azurlewd", "bioshockporn", "bleach_hentai", "rule34disney", "doa_rule34", "fire_emblem_r34",
+          "finalfantasynsfw", "genshinimpactnsfw", "helltakerrule34", "metroid34", "rule34lifeisstrange", "rule34lol",
+          "sexyvelma", "starwarsnsfw", "thelostwoods", "yoko34"]
